5f240aeb5d8df62a2dab33308c7e502e99f8b598
Bryan Jackson <bjack13@gmail.com>
2017-01-08 16:12:29 -0700

    routing debugging

--- public/app.js
+++ public/app.js
@@ -8,6 +8,13 @@ angular.module('app',[])
         url: '/',
         templateUrl: './views/home.html'
       })
-
+      .state('about', {
+        url: '/about',
+        templateUrl: './views/about.html'
+      })
+      .state('other', {
+        url: '/other',
+        templateUrl: './views/other.html'
+      })
 
  });
